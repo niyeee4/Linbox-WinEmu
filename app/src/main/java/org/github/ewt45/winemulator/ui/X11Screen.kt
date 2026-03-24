65e589154de85976323027bc0698efe3d1a017d1
afei <165532911+afeimod@users.noreply.github.com>
2026-03-24 19:42:05 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/ui/X11Screen.kt
+++ app/src/main/java/org/github/ewt45/winemulator/ui/X11Screen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
