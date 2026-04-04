91ed8df73a284904eb5036b66dbbec26e7dde8f8
afei <165532911+afeimod@users.noreply.github.com>
2026-04-04 09:55:34 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,94 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "a.io.github.ewt45.winemulator"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "a.io.github.ewt45.winemulator"
+        minSdk = 26
+        //TODO 考虑升到最新？
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 28
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+//            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+        freeCompilerArgs += listOf(
+            "-Xno-call-assertions",
+            "-Xno-param-assertions",
+            "-Xno-strict-levenshtein-matching"
+        )
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.service)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.org.apache.commons.compress)
+    implementation(libs.org.tukaani.xz)
+    implementation(libs.org.jetbrains.kotlinx.serialization.json)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //termux 终端 。。。jitpack真是垃圾啊
+    implementation(libs.com.termux.terminal.view )
+    implementation(libs.com.termux.termux.shared )
+    implementation(libs.com.google.guava.listenablefuture)
+
+    //termux-x11
+    implementation(files("app-debug.aar"))
+//    implementation(project(":tx11"))
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.preference)
+    implementation(kotlin("reflect"))
+
+
 }
