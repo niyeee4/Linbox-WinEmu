076c9449fe9c02bb9587a1646fba07029e2a9f07
afei <165532911+afeimod@users.noreply.github.com>
2026-04-13 12:52:17 +0800

    Add files via upload

--- app/src/main/java/org/github/ewt45/winemulator/ui/ProotScreen.kt
+++ app/src/main/java/org/github/ewt45/winemulator/ui/ProotScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
